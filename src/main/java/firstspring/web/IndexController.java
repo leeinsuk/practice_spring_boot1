f02a8be3dbc8f3744390b20c3e40d072b464d93f
leeinsuk <42162018+leeinsuk@users.noreply.github.com>
2021-12-20 21:30:12 +0900

    구글 로그인,로그아웃,회원가입,권한관리

--- src/main/java/firstspring/web/IndexController.java
+++ src/main/java/firstspring/web/IndexController.java
@@ -1,6 +1,7 @@
 package firstspring.web;
 
 
+import firstspring.config.auth.dto.SessionUser;
 import firstspring.service.posts.PostsService;
 import firstspring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
 
     @GetMapping("/posts/save")
@@ -24,6 +28,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
