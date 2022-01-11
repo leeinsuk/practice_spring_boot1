ace05bfd18a7ee1ef04d720bd02080329aedf3dd
leeinsuk <42162018+leeinsuk@users.noreply.github.com>
2022-01-11 16:47:55 +0900

    로그인 권한 변경

--- src/main/java/firstspring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/firstspring/config/auth/dto/OAuthAttributes.java
@@ -63,7 +63,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.USER)//#######
+                .role(Role.GUEST)
                 .build();
     }
 }
